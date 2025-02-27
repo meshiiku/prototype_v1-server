3d91c86b135d061d6a06de6e6d37f2836f154158
oto <ottotak@outlook.com>
2025-02-27 21:40:56 +0900

    add: ラーメンの画像とアイコンをアップロード

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,13 @@
 import { Hono } from "hono";
 import accountsApp from "./apps/userApp";
+import { serveStatic } from "hono/bun";
 
 const app = new Hono();
 
 app.get("/", (c) => {
   return c.text("Hello Hono!");
 });
+app.use("/static/*", serveStatic({ root: "./" }));
 app.route("/account", accountsApp);
 
 export default app;
