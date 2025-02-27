3a5521e081b50b948d9405df8be7ed28d01f3621
oto <ottotak@outlook.com>
2025-02-27 18:10:55 +0900

    feat: アカウント登録とuser_idによるアカウントの検索

--- src/apps/userApp.ts
+++ src/apps/userApp.ts
@@ -1,25 +1,14 @@
 import { Hono } from "hono";
-import { sign } from "hono/jwt";
+import { jwt, sign } from "hono/jwt";
+import { prismaClient } from "../constants";
 
 // WARNING: 開発用にハードコーディング
 const JWT_SECRET = "HOGEHOGE";
-const dummy_users = [
-  "taro",
-  "goro",
-  "jiro",
-  "takeru",
-  "masuo",
-  "haneda",
-  "hayate",
-  "genki",
-  "jelly",
-  "neko",
-  "inu",
-];
 
 type JWTPayload = {
   display_name: string;
-  uid: number;
+  id: string;
+  user_id: string;
   exp: number;
 };
 export async function generateJWTToken(
@@ -28,18 +17,79 @@ export async function generateJWTToken(
 ) {
   return await sign(payload, jwt_secret);
 }
-const accountsApp = new Hono();
-accountsApp.get("/dummy", async (c) => {
-  const jwt = await generateJWTToken(
-    {
-      display_name: `${dummy_users[Math.floor(Math.random() * dummy_users.length)]}${Math.floor(Math.random() * 1000)}`,
-      uid: 2,
-      exp: Math.floor(Date.now() / 1000) + 60 * 100, // 100min
-    },
-    JWT_SECRET,
-  );
+function generateRandomUserName() {
+  const dummy_users = [
+    "taro",
+    "goro",
+    "jiro",
+    "takeru",
+    "masuo",
+    "haneda",
+    "hayate",
+    "genki",
+    "jelly",
+    "neko",
+    "inu",
+  ];
+  return `${dummy_users[Math.floor(Math.random() * dummy_users.length)]}${Math.floor(Math.random() * 100000000)}`;
+}
 
-  return c.text(jwt);
-});
+const accountsApp = new Hono();
+accountsApp
+  // jwt認証をかける
+  .use(
+    "/auth/*",
+    jwt({
+      secret: JWT_SECRET,
+    }),
+  )
+  .get("/auth/check", (c) => {
+    return c.text("Ok! you are logged in!");
+  })
+  .get("/users/:user_id", async (c) => {
+    const user = await prismaClient.user.findFirst({
+      where: {
+        user_id: c.req.param("user_id"),
+      },
+    });
+    if (user)
+      return c.json({
+        display_name: user.display_name,
+        id: user.user_id,
+      });
+    else return c.json({});
+  })
+  .get("/anonymous", async (c) => {
+    // 適当にユーザー名を生成する
+    const userName = generateRandomUserName();
+
+    // dbに登録
+    try {
+      const user = await prismaClient.user.create({
+        data: {
+          display_name: `${userName}`,
+          password: `aiueo`,
+          user_id: `${userName}`,
+        },
+      });
+
+      // payloadとシークレットを渡してjwt発行
+      const jwt = await generateJWTToken(
+        {
+          display_name: user.display_name,
+          id: user.id,
+          user_id: user.user_id,
+          exp: Math.floor(Date.now() / 1000) + 60 * 1, // 1min
+        },
+        JWT_SECRET,
+      );
+
+      // jwtを返す
+      return c.text(jwt);
+    } catch {
+      c.text("Error occured", 500);
+    }
+  })
+  .get("/auth/info", async (c) => {});
 
 export default accountsApp;
